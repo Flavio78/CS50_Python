eed468db6d1ec201fe8d8aa95e870c22939e8252
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-02 08:08:35 +0200

    int

--- hello.py
+++ hello.py
@@ -3,3 +3,9 @@ name = input("What's your name? ")
 
 # Say hello to user
 print(f"hello, {name}")
+
+value1 = (int)(input("Insert 1st value "))
+
+value2 = (int)(input(" Insert 2nd value "))
+
+print(f"{value1 + value2}")
