9650e1c641752cd5e3199a9f9ceef89e9c4d569c
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-02 09:23:23 +0200

    type

--- hello.py
+++ hello.py
@@ -1,5 +1,12 @@
-# Ask user for their name
-name = input("What's your name? ")
+# Ask user for their name,
+# remove whitespace from str
+# capitalize user's name
+# split user's name into first name and last name
+[first, last] = input("What's your name? ").strip().title().split(" ")
+
+value: str = 12.34
+
+print(f"type: {type(value)} value: {value}")
 
 # Say hello to user
-print(f"hello, {name}")
+print(f"hello, {first}, {last}")
