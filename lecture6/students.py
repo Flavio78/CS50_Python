34def4ea691fe760e29ddb3ace5709307510f456
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-03 22:34:36 +0200

    Course CS50P Lecture 6: time mark 01:23'45"

--- lecture6/students.py
+++ lecture6/students.py
@@ -1,5 +1,9 @@
+import csv
+
+
 def main():
-    readFile4()
+    # readFile7()
+    writeFile3()
 
 
 def readFile1():
@@ -44,12 +48,64 @@ def readFile4():
         print(f"{student['name']} is in {student['house']}")
 
 
+def readFile5():
+    students = []
+    with open("students_home.csv") as file:
+        for line in file:
+            name, home = line.rstrip().split(",")
+            student = {"name": name, "home": home}
+            students.append(student)
+
+    for student in sorted(students, key=lambda student: student["name"]):
+        print(f"{student['name']} is in {student['home']}")
+
+
 def get_name(student) -> str:
     return student["name"]
 
 
 def get_house(student) -> str:
     return student["house"]
+
+
+def readFile6():
+    students = []
+    with open("students_home.csv") as file:
+        reader = csv.reader(file)
+        for name, home in reader:
+            students.append({"name": name, "home": home})
+
+    for student in sorted(students, key=lambda student: student["name"]):
+        print(f"{student['name']} is from {student['home']}")
+
+
+def readFile7():
+    students = []
+    with open("students_home2.csv") as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            students.append({"name": row["name"], "home": row["home"]})
+
+    for student in sorted(students, key=lambda student: student["name"]):
+        print(f"{student['name']} is from {student['home']}")
+
+
+def writeFile2():
+    name = input("What's your name? ")
+    home = input("What's your home? ")
+
+    with open("students2.csv", "a") as file:
+        writer = csv.writer(file)
+        writer.writerow([name, home])
+
+
+def writeFile3():
+    name = input("What's your name? ")
+    home = input("What's your home? ")
+
+    with open("students3.csv", "a", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=["name", "home"])
+        writer.writerow({"name": name, "home": home})
 
 
 if __name__ == "__main__":
