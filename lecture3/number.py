6d77b8cc457dbfdc716f9235776c345b5ce52c57
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-03 14:12:56 +0200

    Course CS50P Lecture 3: end

--- lecture3/number.py
+++ lecture3/number.py
@@ -1,5 +1,7 @@
 def main():
-    num2()
+    # num4()
+    x = get_int4("What's x? ")
+    print(f"x is {x}")
 
 
 def num1():
@@ -18,6 +20,68 @@ def num2():
         print("x is not an integer")
     else:
         print(f"x is {x}")
+
+
+def num3():
+    while True:
+        try:
+            x = int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    print(f"x is {x}")
+
+
+def num4():
+    while True:
+        try:
+            x = int(input("What's x? "))
+            break
+        except ValueError:
+            print("x is not an integer")
+    print(f"x is {x}")
+
+
+def get_int1():
+    while True:
+        try:
+            x = int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+        else:
+            break
+    return x
+
+
+def get_int2() -> int:
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            print("x is not an integer")
+
+
+def get_int3() -> int:
+    """
+    Get value from input until an integer is entered
+    """
+    while True:
+        try:
+            return int(input("What's x? "))
+        except ValueError:
+            pass
+
+
+def get_int4(prompt: str = "promt") -> int:
+    """
+    Get value from input until an integer is entered
+    """
+    while True:
+        try:
+            return int(input(prompt))
+        except ValueError:
+            pass
 
 
 main()
