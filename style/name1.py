f3c8cb1f7314bdbedc7bbe0debd7dfe9b9b2a531
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-03 19:24:46 +0200

    Course CS50P Lecture 5: end

---
+++ style/name1.py
@@ -0,0 +1,23 @@
+"""Name1 file"""
+
+
+def main():
+    """
+    Main function.
+
+    Parameters:
+    None.
+
+    Returns:
+    None.
+    """
+    function1()
+
+
+def function1():
+    """Function 1"""
+    print()
+
+
+if __name__ == "__main__":
+    main()
---
+++ style/students.py
@@ -0,0 +1,12 @@
+"""
+Students
+"""
+stundents = {
+    "Hermione": "Griffondor",
+    "Harry": "Griffondor",
+    "Ron": "Griffondor",
+    "Draco": "Slytherin",
+    "Padma": "Ravenclaw",
+}
+for stundent in stundents:
+    print(stundent)
