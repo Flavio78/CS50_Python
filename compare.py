a088340b17a9a3555c56e2a92f07c68fbdc3f1bc
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-03 08:41:19 +0200

    Lecture 1: end

--- compare.py
+++ compare.py
@@ -2,6 +2,10 @@ def main():
     x = int(input("What's x? "))
     y = int(input("What's y? "))
 
+    if x!= y:
+        print(f"{x} is not equal to {y}")
+    else:
+        print(f"{x} is equal to {y}")
     if x < y:
         print(f"{x} is less than {y}")
     elif x > y:
