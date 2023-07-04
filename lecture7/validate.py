eb88a6c788a2fe3593420e9f52b5e6fd9a8a1b25
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-04 23:07:33 +0200

    Course CS50P Lecture 7: time mark 53':15"

--- lecture7/validate.py
+++ lecture7/validate.py
@@ -1,5 +1,14 @@
+import re
+import sys
+
+
 def main():
-    email = input("What's your email? ").strip()
+    ival = intArray()
+    for v in ival:
+        print(v)
+
+    email = input("What's your email? ").strip() if len(sys.argv) != 2 else sys.argv[1]
+
     if validate1(email):
         print("Valid")
     else:
@@ -10,8 +19,19 @@ def validate1(email: str) -> bool:
     """
     Validate string as email
     """
-    username, domain = email.split("@")
-    return username and domain.endswith(".edu")
+    # username, domain = email.split("@")
+    # return username and domain.endswith(".edu")
+    # r for raw string, that is without escaping \
+    regex1 = "^[^@]+@[^@]+\.edu$"
+    regex2 = "^[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.edu$"
+    regex3 = "^\w+@\w+\.(com|edu|gov|net|org)$"
+    return True if re.search(rf"{regex3}", email) else False
+
+
+def intArray() -> list[int]:
+    x: list[int] = []
+    x.append(1)
+    return x
 
 
 if __name__ == "__main__":
