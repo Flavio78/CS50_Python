1ea521628bf8a23e7c01d81e9776461e2e6ae0e5
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-07 19:28:59 +0200

    Course CS50P Lecture 8: time mark 01:37'07"

--- lecture8/student.py
+++ lecture8/student.py
@@ -4,21 +4,25 @@ class Student:
     By default class is immutable
     """
 
-    def __init__(self, name, house):
+    def __init__(self, name, house, patronus=None):
         """
         implement of the initialization in Python
         self get access to the object I have already created
         With self.name I create a new method
         Something different from creator function from other languages.
         self.name and self.house are created inside the object
+        Patronus is optional
         """
+        # I validate the data inside the class, keep all the related code together
         if not name:  # if user pass an empty string
             # no return None because it is too late: the object is already been created
             raise ValueError("Missing name")  # I raise Excetion
-        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
-            raise ValueError("Invalid house")
+        # if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+        #     raise ValueError("Invalid house")
         self.name = name
-        self.house = house
+        self.house = house  # is going to called the setter method so no need to RaiseError for house
+        # I use house and _house because I want to have the error checking also in initialization
+        self.patronus = patronus
 
     def __str__(self):
         """
@@ -26,12 +30,37 @@ class Student:
         """
         return f"{self.name} from {self.house}"
 
+    def charm(self):
+        """
+        Custom method for this particular object.
+        It is method inside a class so, to access to the members fof the object, I ahve to pass self
+        """
+        match self.patronus:
+            case "Stag":
+                return "🐎"
+            case "Otter":
+                return "🦦"
+            case "Jack Russel Terrier":
+                return "🐶"
+            case _:
+                return "🪄"
 
-# I validate the data inside the class, keep all the related code together
+    # Getter: get attribute from a class
+    # _house is my instance variable name
+    @property
+    def house(self):
+        return self._house
+
+    # Setter: set value for an attribut from a class
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Revenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house  # self.house would be in conflict with the property house
 
 
 def main():
-    function7()
+    function9()
 
 
 def function1():
@@ -68,13 +97,28 @@ def function5():
     print(f"{student['name']} from {student['house']}")
 
 
-def function6():
-    student = get_student6()
-    print(f"{student.name} from {student.house}")
+# def function6():
+#     student = get_student6()
+#     print(f"{student.name} from {student.house}")
 
 
 def function7():
     student = get_student7()
+    print(student)
+
+
+def function8():
+    student = get_student8()
+    print(student)
+    print("Expecto Patronum!")
+    print(student.charm())
+
+
+def function9():
+    student = get_student9()
+    # Setter for house is called
+    # I prevent the data from wrong incorrent input
+    # student.house = ("Number Four, Privet Drive")
     print(student)
 
 
@@ -125,14 +169,27 @@ def get_student5():
     return {"name": name, "house": house}
 
 
-def get_student6():
-    student = Student()
-    student.name = input("Name: ")
-    student.house = input("House: ")
-    return student
+# def get_student6():
+#     student = Student()
+#     student.name = input("Name: ")
+#     student.house = input("House: ")
+#     return student
 
 
 def get_student7():
+    name = input("Name: ")
+    house = input("House: ")
+    return Student(name, house)
+
+
+def get_student8():
+    name = input("Name: ")
+    house = input("House: ")
+    patronus = input("Patronus: ")
+    return Student(name, house, patronus)
+
+
+def get_student9():
     name = input("Name: ")
     house = input("House: ")
     return Student(name, house)
