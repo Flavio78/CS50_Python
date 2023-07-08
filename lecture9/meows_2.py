c03771009046f8f1b6afdb10dc6815f155a53765
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-08 15:42:28 +0200

    Course CS50P lecture 8: time mark 00:55':12"

--- lecture9/meows_2.py
+++ lecture9/meows_2.py
@@ -1,10 +1,35 @@
 """New version for mypy"""
 
 
-def meow(n: int) -> None:
+def meow1(n: int) -> None:
+    """
+    Meow n times. v.1
+    :param n: Number of times to meow
+    :type n: int
+    """
     for _ in range(n):
         print("meow")
 
 
-number: int = int(input("Number: "))
-meow(number)
+def meow2(n: int) -> str:
+    """
+    Meow n times.
+
+    :param n: Number of times to meow
+    :type n: int
+    :raise TypeError: If n is not an int
+    :return: A string of n meows, one per line
+    :rtype: str
+    """
+    return "meow\n" * n  # multiplication concatenates strings
+
+
+def main() -> None:
+    """Main function."""
+    number: int = int(input("Number: "))
+    meows: str = meow2(number)
+    print(meows, end="")
+
+
+if __name__ == "__main__":
+    main()
