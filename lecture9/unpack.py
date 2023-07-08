eb9b96acc3e0363e5d795cc18627b1cce9dad8f6
Flavio78 <53538168+Flavio78@users.noreply.github.com>
2023-07-08 17:23:30 +0200

    Course CS50P lecture 8: time mark 01:27':49"

--- lecture9/unpack.py
+++ lecture9/unpack.py
@@ -4,10 +4,14 @@
 def main() -> None:
     # function1()
     # print(total(100, 50, 25), "Knuts")
-    coins = [100, 50, 25]
+    coins1 = [100, 50, 25]
     # print(total(coins[0], coins[1], coins[2]), "Knuts")
     # with *coins I pass the individual elements of the list
-    print(total(*coins), "Knuts")
+    print(total(*coins1), "Knuts")
+    coins2 = {"galleons": 100, "sickles": 50, "knuts": 25}
+    print(total(coins2["galleons"], coins2["sickles"], coins2["knuts"]), "Knuts")
+    # passing arguments with names
+    print(total(**coins2), "Knuts")
 
 
 def function1() -> None:
